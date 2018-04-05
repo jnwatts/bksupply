b4f4493fa2410cdfae85192326054bd55dddad8f
Josh Watts <josh+github@sroz.net>
2018-04-05 09:56:06 -0400

    Mainwindow: Implement new buttons and add QSettings

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,5 @@
+#include <QSettings>
+#include <QFileDialog>
 #include <QDebug>
 #include <iostream>
 #include <QDateTime>
@@ -11,6 +13,7 @@ static BK1696 bk;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
+    _settings("SROZ", "bksupply"),
     _timer(this)
 {
     ui->setupUi(this);
@@ -29,14 +32,9 @@ MainWindow::MainWindow(QWidget *parent) :
             this->_timer.stop();
     });
 
-    foreach (const QString &port, BK1696::ports()) {
-        this->ui->port->addItem(port);
-    }
-    this->ui->port->setCurrentIndex(0);
+    this->loadSettings();
 
     this->_timer.setInterval(250);
-
-    this->update();
 }
 
 MainWindow::~MainWindow()
@@ -126,5 +124,106 @@ void MainWindow::update()
     this->ui->off->setEnabled(bk.isOpen());
     this->ui->open->setEnabled(!bk.isOpen());
     this->ui->close->setEnabled(bk.isOpen());
+    this->ui->port->setEnabled(!bk.isOpen());
+    this->ui->addPort->setEnabled(!bk.isOpen());
+    this->ui->delPort->setEnabled(!bk.isOpen() && this->isSelectedPortCustom());
+}
 
+void MainWindow::updatePorts(void)
+{
+    this->ui->port->clear();
+    for (int i = 0; i < this->_customPorts.size(); ++i) {
+        this->ui->port->addItem(this->_customPorts.at(i), QVariantList({"CUSTOM", i}));
+    }
+    for (int i = 0; i < BK1696::ports().size(); ++i) {
+        this->ui->port->addItem(BK1696::ports().at(i), QVariantList({"BK", i}));
+    }
+}
+
+void MainWindow::addCustomPort(QString port)
+{
+    this->_customPorts << port;
+    this->saveSettings();
+    this->updatePorts();
+}
+
+void MainWindow::removeCustomPort(int index)
+{
+    this->_customPorts.remove(index);
+    this->saveSettings();
+    this->updatePorts();
+}
+
+bool MainWindow::isSelectedPortCustom()
+{
+    if (this->ui->port->currentIndex() < 0)
+        return false;
+    QVariantList selectedItem = this->ui->port->currentData().toList();
+    QString type = selectedItem.at(0).toString();
+    return (type == "CUSTOM");
+}
+
+void MainWindow::saveSettings(void)
+{
+    QSettings &settings = this->settings();
+    settings.beginWriteArray("custom_ports");
+    for (int i = 0; i < this->_customPorts.size(); ++i) {
+        settings.setArrayIndex(i);
+        settings.setValue("path", this->_customPorts.at(i));
+    }
+    settings.endArray();
+    settings.setValue("selected_port", this->ui->port->currentIndex());
+}
+
+void MainWindow::loadSettings(void)
+{
+    QSettings &settings = this->settings();
+    int size;
+
+    size = settings.beginReadArray("custom_ports");
+    for (int i = 0; i < size; ++i) {
+        settings.setArrayIndex(i);
+        QString port = settings.value("path", "").toString();
+        if (port.size() > 0)
+            this->_customPorts << port;
+    }
+    settings.endArray();
+    this->updatePorts();
+
+    int selected_port = settings.value("selected_port", 0).toInt();
+    if (selected_port >= this->ui->port->count() || selected_port < 0)
+        selected_port = 0;
+    this->ui->port->setCurrentIndex(selected_port);
+
+    this->update();
+}
+
+void MainWindow::on_addPort_clicked()
+{
+    QFileDialog dialog;
+    dialog.setFileMode(QFileDialog::ExistingFile);
+    dialog.setResolveSymlinks(false);
+    if (dialog.exec())
+        this->addCustomPort(dialog.selectedFiles().at(0));
+}
+
+void MainWindow::on_delPort_clicked()
+{
+    if (this->ui->port->currentIndex() < 0)
+        return;
+    QVariantList selectedItem = this->ui->port->currentData().toList();
+    QString type = selectedItem.at(0).toString();
+    int index = selectedItem.at(1).toInt();
+
+    if (type == "CUSTOM") {
+        this->removeCustomPort(index);
+    }
+}
+
+void MainWindow::on_port_currentIndexChanged(int index)
+{
+    if (index < 0)
+        return;
+
+    this->update();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,9 +1,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QSettings>
 #include <QMainWindow>
 #include <QTimer>
 #include <QLabel>
+#include <QVector>
 
 namespace Ui {
 class MainWindow;
@@ -25,13 +27,25 @@ private slots:
     void on_on_clicked();
     void on_off_clicked();
     void on_clear_clicked();
+    void on_addPort_clicked();
+    void on_delPort_clicked();
+    void on_port_currentIndexChanged(int index);
 
 private:
     void display(QLabel *label, double value, int precision);
     void update(void);
+    void updatePorts(void);
+    void addCustomPort(QString port);
+    void removeCustomPort(int index);
+    bool isSelectedPortCustom(void);
+    void saveSettings(void);
+    void loadSettings(void);
+    QSettings &settings(void) { return this->_settings; }
 
     Ui::MainWindow *ui;
+    QSettings _settings;
     QTimer _timer;
+    QVector<QString> _customPorts;
 };
 
 #endif // MAINWINDOW_H
